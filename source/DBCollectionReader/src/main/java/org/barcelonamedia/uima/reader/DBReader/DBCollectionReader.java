d85f9668e8e920b28ca8b6dd754cd333b65c1e5b
Jens Grivolla <jens.grivolla@barcelonamedia.org>
2014-02-24 17:21:38 +0100

    changed PARAM_* definitions to public (mainly for use with UIMAfit)

--- source/DBCollectionReader/src/main/java/org/barcelonamedia/uima/reader/DBReader/DBCollectionReader.java
+++ source/DBCollectionReader/src/main/java/org/barcelonamedia/uima/reader/DBReader/DBCollectionReader.java
@@ -36,127 +36,127 @@ import org.barcelonamedia.uima.reader.DBReader.DAO.DocumentDAO;
 
 
 public class DBCollectionReader extends CollectionReader_ImplBase{
-	
+
 	/** The logger object. */
 	private static final Logger logger = Logger.getLogger(DBCollectionReader.class.toString());
-	
+
 	// Suported DBMS:  -----------------------------------------
-	
+
 	private static final String MySQL = "MySQL";
-	
+
 	//----------------------------------------------------------
-	
-	
+
+
 	/** Correponds to a parameter that specifies DBMS to be used.
 	 * The value of this variable is 'DBMS' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_DBMS = "DBMS";
-	
+	public static final String PARAM_DBMS = "DBMS";
+
 	/** Correponds to a parameter that specifies the server where DBMS is being hosted.
 	 * The value of this variable is 'Server' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_SERVER = "Server";
-	
+	public static final String PARAM_SERVER = "Server";
+
 	/** Correponds to a parameter that specifies port to be used to connect to the specified DBMS.
 	 * The value of this variable is 'Port' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_PORT = "Port";
-	
+	public static final String PARAM_PORT = "Port";
+
 	/** Correponds to a parameter that specifies the name of the database to be used.
 	 * The value of this variable is 'Database' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_DATABASE = "Database";
-	
+	public static final String PARAM_DATABASE = "Database";
+
 	/** Correponds to a parameter that specifies the username fof the specified database.
 	 * The value of this variable is 'User' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_USER = "User";
-	
-	/** Correponds to a parameter that specifies the password fof the specified database.
+	public static final String PARAM_USER = "User";
+
+	/** Correponds to a parameter that specifies the password of the specified database.
 	 * The value of this variable is 'Password' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_PASSWORD = "Password";
-	
-	
-	/** Correponds to an optional parameter that contains the language of the documents in the database.
-	 * If not specified, the default system encoding will be used.
+	public static final String PARAM_PASSWORD = "Password";
+
+
+	/** Correponds to a parameter that specifies the sql statement to be executed in order to 
+	 * retrieve the proper information.
 	 * The value of this variable is 'sql_select' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_SQL = "sql_select";
-	
+	public static final String PARAM_SQL = "sql_select";
 
-	/** Correponds to a parameter that specifies the sql statement to be executed in order to 
-	 * retrieve the proper information.
+
+	/** Correponds to an optional parameter that contains the language of the documents in the database.
+	 * If not specified, the default system encoding will be used.
 	 * The value of this variable is 'Language' which is the name of 
 	 * the parameter in the descriptor file that must be set.
 	 * @see "/DBCollectionReader/desc/DBCollectionReader"
 	 **/
-	private static final String PARAM_LANGUAGE = "Language";
-	
+	public static final String PARAM_LANGUAGE = "Language";
+
 	/** DAO Factory object. */
 	private DAOFactory daoFactory;
-	
+
 	/** XMI DAO object. */
 	private DocumentDAO documentDAO;
-	
+
 	private ResultSet documents;
-	
+
 	private int documentSize;
-	
+
 	private String language;
-	
+
 	private int documentCounter;
-	
+
 	/** ID of the document in process **/
 	private String documentID;
 
 	/** Document in process **/
 	private String documentData;
-	
-	
+
+
 	/**
-     * Initialize the component. Retrieve the parameters and process them, 
-     * parsing the field descriptions and preparing the structures needed to
-     * process the documents.
-     *
-     * @param aContext
-     *            The UIMA context.
-     *
-     * @throws ResourceInitializationException
-     *             If an error occurs with some resource.
-     *
-     * @see org.apache.uima.analysis_component.AnalysisComponent_ImplBase#initialize(org.apache.uima.UimaContext)
-     */
+	 * Initialize the component. Retrieve the parameters and process them, 
+	 * parsing the field descriptions and preparing the structures needed to
+	 * process the documents.
+	 *
+	 * @param aContext
+	 *            The UIMA context.
+	 *
+	 * @throws ResourceInitializationException
+	 *             If an error occurs with some resource.
+	 *
+	 * @see org.apache.uima.analysis_component.AnalysisComponent_ImplBase#initialize(org.apache.uima.UimaContext)
+	 */
 	public void initialize() throws ResourceInitializationException {
-		
+
 		System.out.println("DBCollectionReader: initialize()...");
 		logger.info("initialize()...");
-		
+
 		String dbms = (String) getUimaContext().getConfigParameterValue(PARAM_DBMS);
 		String server = (String) getUimaContext().getConfigParameterValue(PARAM_SERVER);
 		int port = (Integer) getUimaContext().getConfigParameterValue(PARAM_PORT);
 		String database = (String) getUimaContext().getConfigParameterValue(PARAM_DATABASE);
 		String user = (String) getUimaContext().getConfigParameterValue(PARAM_USER);
 		String password = (String) getUimaContext().getConfigParameterValue(PARAM_PASSWORD);
-		
+
 		String sql_sentence = (String) getUimaContext().getConfigParameterValue(PARAM_SQL);
-		
+
 		this.language = (String) getUimaContext().getConfigParameterValue(PARAM_LANGUAGE);	
-		
+
 		if((dbms == null || dbms.length() == 0) || 
 				(server == null || server.length() == 0) || 
 				(new Integer(port) == null) || 
@@ -164,148 +164,148 @@ public class DBCollectionReader extends CollectionReader_ImplBase{
 				(user == null || user.length() == 0) || 
 				(password == null || password.length() == 0) || 
 				(sql_sentence == null || sql_sentence.length() == 0)){
-			
+
 			throw new ResourceInitializationException();
 		}
-		
+
 		logger.info("initialize() - dbms: " + dbms);
 		logger.info("initialize() - server: " + server);
 		logger.info("initialize() - port: " + port);
 		logger.info("initialize() - database: " + database);
 		logger.info("initialize() - user: " + user);
 		logger.info("initialize() - password: " + password);
-		
+
 		logger.info("initialize() - sql_sentence: " + sql_sentence);
-		
+
 		if(dbms.equals(MySQL)){
-			
+
 			System.out.println("DBCollectionReader: initialize() - Using MySQL as DBMS.");
-		
+
 			this.daoFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
-			
+
 			Hashtable<String, String> connectionParams = new Hashtable<String, String>();
 			connectionParams.put("server", server);
 			connectionParams.put("port", String.valueOf(port));
 			connectionParams.put("database", database);
 			connectionParams.put("user", user);
 			connectionParams.put("password", password);
-			
+
 			this.documentDAO = this.daoFactory.getDocumentDAO(connectionParams);
-			
+
 			this.documentDAO.setSQLSentence(sql_sentence);
-			
+
 			try{
-				
+
 				this.documents = this.documentDAO.getDocumentsText();				
 				this.documentSize = this.documentDAO.getNumberOfDocumentsText();
 			}
 			catch (DAOException e){
-				
+
 				throw new ResourceInitializationException(e);
 			}
 		}
-		
+
 		this.documentCounter = 0;
-		
+
 		logger.info("initialize() - Done.");
 	}
 
 	public void getNext(CAS aCAS) throws IOException, CollectionException {
-		
+
 		JCas jcas;
-		
-	    try{
-	    	
-	      jcas = aCAS.getJCas();
-	    }
-	    catch(CASException e){
-	    	
-	      throw new CollectionException(e);
-	    }
-	    
-	     // put document in CAS
-	    jcas.setDocumentText(documentData);
 
-	    // set language if it was explicitly specified as a configuration parameter
-	    if(this.language != null){
-	    	
-	      jcas.setDocumentLanguage(this.language);
-	    }
+		try{
 
-	    // Also store location of source document in CAS. This information is critical
-	    // if CAS Consumers will need to know where the original document contents are located.
-	    // For example, the Semantic Search CAS Indexer writes this information into the
-	    // search index that it creates, which allows applications that use the search index to
-	    // locate the documents that satisfy their semantic queries.
-	    SourceDocumentInformation srcDocInfo = new SourceDocumentInformation(jcas);
-	    srcDocInfo.setUri("file:///" + this.documentID); // needs to be a valid URI with a known protocol
-	    srcDocInfo.setOffsetInSource(0);
-	    srcDocInfo.setDocumentSize((int) this.documentData.length());
-	    
-	    this.documentCounter += 1;
-	    
-	    srcDocInfo.setLastSegment(true);
-	    srcDocInfo.addToIndexes();
+			jcas = aCAS.getJCas();
+		}
+		catch(CASException e){
+
+			throw new CollectionException(e);
+		}
+
+		// put document in CAS
+		jcas.setDocumentText(documentData);
+
+		// set language if it was explicitly specified as a configuration parameter
+		if(this.language != null){
+
+			jcas.setDocumentLanguage(this.language);
+		}
+
+		// Also store location of source document in CAS. This information is critical
+		// if CAS Consumers will need to know where the original document contents are located.
+		// For example, the Semantic Search CAS Indexer writes this information into the
+		// search index that it creates, which allows applications that use the search index to
+		// locate the documents that satisfy their semantic queries.
+		SourceDocumentInformation srcDocInfo = new SourceDocumentInformation(jcas);
+		srcDocInfo.setUri("file:///" + this.documentID); // needs to be a valid URI with a known protocol
+		srcDocInfo.setOffsetInSource(0);
+		srcDocInfo.setDocumentSize((int) this.documentData.length());
+
+		this.documentCounter += 1;
+
+		srcDocInfo.setLastSegment(true);
+		srcDocInfo.addToIndexes();
 	}
 
 	public boolean hasNext() throws IOException, CollectionException {
 
-		
+
 		try{
-			
+
 			boolean hasNext = this.documents.next();
-			
+
 			if(!hasNext){
-				
+
 				this.documentDAO.closeConnection();
 			}
 			else{
-				
+
 				//get document 
-			    this.documentData = this.documents.getString("text");			
-			    
-				 //get document id
+				this.documentData = this.documents.getString("text");			
+
+				//get document id
 				this.documentID = this.documents.getString("id");				
 			}
 
 			return hasNext;
 		}
 		catch(Exception e){
-			
+
 			try{
-		
+
 				// try to reconnect and continue.
 				this.documentDAO.getDocumentsTextFrom(this.documentID);
 				boolean hasNext = this.documents.next();
-				
+
 				if(!hasNext){
-					
+
 					this.documentDAO.closeConnection(); 
 					return false;
 				}
-			    
+
 				//get document 
-			    this.documentData = this.documents.getString("text");			
-			    
-				 //get document id
+				this.documentData = this.documents.getString("text");			
+
+				//get document id
 				this.documentID = this.documents.getString("id");	
-				
+
 				return true;
 			}
 			catch(Exception E){
-				
-		    	System.err.println("ERROR in reconnect");
+
+				System.err.println("ERROR in reconnect");
 				throw new CollectionException(E);
 			}
 		}
 	}
 
 	public Progress[] getProgress(){
-		
+
 		return new Progress[] { new ProgressImpl(this.documentCounter, this.documentSize, Progress.ENTITIES) };
 	}
 
 	public void close() throws IOException {
-		
+
 	}
 }
